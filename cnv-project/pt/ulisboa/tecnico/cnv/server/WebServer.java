73e986d5d07f03c6050b6e535aeb336314f62759
Miguel Leve <miguel.levezinho@tecnico.ulisboa.pt>
2021-05-20 00:11:55 +0100

    Added Health check handler stub to webserver
    - When query is empty

--- cnv-project/pt/ulisboa/tecnico/cnv/server/WebServer.java
+++ cnv-project/pt/ulisboa/tecnico/cnv/server/WebServer.java
@@ -59,104 +59,115 @@ public class WebServer {
 
       final String query = t.getRequestURI().getQuery();
 
-      Statistics ss = new Statistics(query);
+      if (query == null) {
+        final String response = "ping";
 
-      statistics.put(Thread.currentThread().getId(), new Statistics(query));
+        t.sendResponseHeaders(200, 0);
+        OutputStream pingOs = t.getResponseBody();
+        pingOs.write(response.getBytes());
+        pingOs.close();
 
-      System.out.println("> Query:\t" + query);
+        System.out.println("> Sent response to " + t.getRemoteAddress().toString());
+      } else {
+        Statistics ss = new Statistics(query);
 
-      final String[] params = query.split("&");
-      final ArrayList<String> newArgs = new ArrayList<>();
+        statistics.put(Thread.currentThread().getId(), new Statistics(query));
 
-      for (final String p : params) {
-        final String[] splitParam = p.split("=");
+        System.out.println("> Query:\t" + query);
 
-        if (splitParam[0].equals("i")) {
-          splitParam[1] = WebServer.sap.getMapsDirectory() + "/" + splitParam[1];
+        final String[] params = query.split("&");
+        final ArrayList<String> newArgs = new ArrayList<>();
+
+        for (final String p : params) {
+          final String[] splitParam = p.split("=");
+
+          if (splitParam[0].equals("i")) {
+            splitParam[1] = WebServer.sap.getMapsDirectory() + "/" + splitParam[1];
+          }
+
+          newArgs.add("-" + splitParam[0]);
+          newArgs.add(splitParam[1]);
         }
 
-        newArgs.add("-" + splitParam[0]);
-        newArgs.add(splitParam[1]);
-      }
-
-      if (sap.isDebugging()) {
-        newArgs.add("-d");
-      }
-
-      final String[] args = new String[newArgs.size()];
-      int i = 0;
-
-      for (String arg : newArgs) {
-        args[i] = arg;
-        i++;
-      }
-
-			final Solver s = SolverFactory.getInstance().makeSolver(args);
-
-      if (s == null) {
-        System.out.println("> Problem creating Solver. Exiting.");
-        System.exit(1);
-      }
-
-      File responseFile = null;
-      try {
-        final BufferedImage outputImg = s.solveImage();
-        final String outPath = WebServer.sap.getOutputDirectory();
-        final String imageName = s.toString();
-        final Path imagePathPNG = Paths.get(outPath, imageName);
-
-        ImageIO.write(outputImg, "png", imagePathPNG.toFile());
-
-        responseFile = imagePathPNG.toFile();
-
-      } catch (final FileNotFoundException e) {
-        e.printStackTrace();
-      } catch (final IOException e) {
-        e.printStackTrace();
-      } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-
-      final Headers hdrs = t.getResponseHeaders();
-
-      hdrs.add("Content-Type", "image/png");
-      hdrs.add("Access-Control-Allow-Origin", "*");
-      hdrs.add("Access-Control-Allow-Credentials", "true");
-      hdrs.add("Access-Control-Allow-Methods", "POST, GET, HEAD, OPTIONS");
-      hdrs.add("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-
-      t.sendResponseHeaders(200, responseFile.length());
-
-      final OutputStream os = t.getResponseBody();
-      Files.copy(responseFile.toPath(), os);
-
-      os.close();
-
-      try {
-        File file = new File("./statistics.txt");
-
-        if (!file.exists()) {
-          file.createNewFile();
+        if (sap.isDebugging()) {
+          newArgs.add("-d");
         }
 
-        final PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(file, true)));
-        final Statistics currStatistics = statistics.get(Thread.currentThread().getId());
+        final String[] args = new String[newArgs.size()];
+        int i = 0;
 
-        writer.println(currStatistics.getQuery());
-        writer.println(currStatistics.getICount());
-        writer.println(currStatistics.getMCount());
+        for (String arg : newArgs) {
+          args[i] = arg;
+          i++;
+        }
 
-        writer.close();
+        final Solver s = SolverFactory.getInstance().makeSolver(args);
 
-      } catch (Exception e) {
-        System.out.println("Error!!!!");
+        if (s == null) {
+          System.out.println("> Problem creating Solver. Exiting.");
+          System.exit(1);
+        }
+
+        File responseFile = null;
+        try {
+          final BufferedImage outputImg = s.solveImage();
+          final String outPath = WebServer.sap.getOutputDirectory();
+          final String imageName = s.toString();
+          final Path imagePathPNG = Paths.get(outPath, imageName);
+
+          ImageIO.write(outputImg, "png", imagePathPNG.toFile());
+
+          responseFile = imagePathPNG.toFile();
+
+        } catch (final FileNotFoundException e) {
+          e.printStackTrace();
+        } catch (final IOException e) {
+          e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+          e.printStackTrace();
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+
+        final Headers hdrs = t.getResponseHeaders();
+
+        hdrs.add("Content-Type", "image/png");
+        hdrs.add("Access-Control-Allow-Origin", "*");
+        hdrs.add("Access-Control-Allow-Credentials", "true");
+        hdrs.add("Access-Control-Allow-Methods", "POST, GET, HEAD, OPTIONS");
+        hdrs.add("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
+        t.sendResponseHeaders(200, responseFile.length());
+
+        final OutputStream os = t.getResponseBody();
+        Files.copy(responseFile.toPath(), os);
+
+        os.close();
+
+        try {
+          File file = new File("./statistics.txt");
+
+          if (!file.exists()) {
+            file.createNewFile();
+          }
+
+          final PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(file, true)));
+          final Statistics currStatistics = statistics.get(Thread.currentThread().getId());
+
+          writer.println(currStatistics.getQuery());
+          writer.println(currStatistics.getICount());
+          writer.println(currStatistics.getMCount());
+
+          writer.close();
+
+        } catch (Exception e) {
+          System.out.println("Error!!!!");
+        }
+
+        statistics.remove(Thread.currentThread().getId());
+
+        System.out.println("> Sent response to " + t.getRemoteAddress().toString());
       }
-
-      statistics.remove(Thread.currentThread().getId());
-
-      System.out.println("> Sent response to " + t.getRemoteAddress().toString());
     }
   }
 
