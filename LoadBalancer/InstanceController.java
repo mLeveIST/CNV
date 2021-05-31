e2d704873d6a1a8c9892a1c4d0fedf50c8a82c78
Miguel Leve <miguel.levezinho@tecnico.ulisboa.pt>
2021-05-31 07:19:40 +0100

    Added DB querying to LB

--- LoadBalancer/InstanceController.java
+++ LoadBalancer/InstanceController.java
@@ -11,6 +11,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
@@ -22,6 +23,9 @@ import com.amazonaws.services.cloudwatch.model.Dimension;
 import com.amazonaws.services.cloudwatch.model.GetMetricStatisticsRequest;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.*;
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
 import com.amazonaws.services.ec2.model.*;
@@ -33,11 +37,16 @@ import com.sun.net.httpserver.HttpServer;
 public class InstanceController {
     private static final int MIN_INSTANCES = 1;
     private static final int MAX_INSTANCES = 10;
+    private static final String TABLE_NAME = "Requests_Info";
+
+    private static final String GRID_SCAN = "GRID_SCAN";
+    private static final String GREEDY_RANGE_SCAN = "GREEDY_RANGE_SCAN";
+    private static final String PROGRESSIVE_SCAN = "PROGRESSIVE_SCAN";
 
     private static ControllerArgumentParser controllerArgumentParser;
 
     private static AmazonCloudWatch cloudWatch;
-    private static AmazonDynamoDB dynamoDB;
+    private static DynamoDB dynamoDB;
     private static AmazonEC2 ec2;
 
     private static Map<String, InstanceInfo> instances;
@@ -56,10 +65,10 @@ public class InstanceController {
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .build();
 
-        dynamoDB = AmazonDynamoDBClientBuilder.standard()
+        dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion("us-east-1")
-                .build();
+                .build());
 
         ec2 = AmazonEC2ClientBuilder.standard()
                 .withRegion("us-east-1")
@@ -100,13 +109,14 @@ public class InstanceController {
 
             Instance instance = runInstancesResult.getReservation().getInstances().get(0);
 
-            System.out.println("> Created instance with ID " + instance.getInstanceId());
+            System.out.println("[AS] > Created instance with ID " + instance.getInstanceId());
 
             while (!instance.getState().getName().equals("running")) {
+                System.out.println("[AS] > Waiting for instance with ID " + instance.getInstanceId());
                 Thread.sleep(2000);
             }
 
-            System.out.println("> Running instance with ID " + instance.getInstanceId());
+            System.out.println("[AS] > Running instance with ID " + instance.getInstanceId());
 
             instances.put(instance.getInstanceId(), new InstanceInfo(instance));
 
@@ -168,7 +178,7 @@ public class InstanceController {
             }
 
             if (numDataPoints == 0) {
-                System.out.println("> WARNING : No data points for instance " + instance.getInstanceId());
+                System.out.println("[AS] > WARNING : No data points for instance " + instance.getInstanceId());
                 continue;
             }
 
@@ -187,7 +197,7 @@ public class InstanceController {
         if (totalAverageCPU >= 0.8) {
             createInstance();
         } else if (numInstances > 1 && totalAverageCPU <= 0.3 && lowestInstance != null) {
-            System.out.println("> Flagging to Terminate instance " + lowestInstance.getInstanceId());
+            System.out.println("[AS] > Flagging to Terminate instance " + lowestInstance.getInstanceId());
             lowestInstance.setPendingTermination();
             terminateInstance(lowestInstance);
         }
@@ -195,7 +205,7 @@ public class InstanceController {
 
     private static void terminateInstance(final InstanceInfo instanceInfo) {
         if (instanceInfo == null) {
-            System.out.println("> ERROR : Instance to terminate is NULL");
+            System.out.println("[AST] > ERROR : Instance to terminate is NULL");
             return;
         }
 
@@ -203,19 +213,23 @@ public class InstanceController {
             @Override
             public void run() {
                 try {
-                    while (instanceInfo.getNumberOfPendingRequests() != 0)
+                    while (instanceInfo.getNumberOfPendingRequests() != 0) {
+                        System.out.println("[AST] > Waiting for requests on instance " + instanceInfo.getInstanceId());
                         Thread.sleep(5000);
+                    }
 
-                    System.out.println("> Terminating instance " + instanceInfo.getInstanceId());
+                    System.out.println("[AST] > Terminating instance " + instanceInfo.getInstanceId());
 
                     TerminateInstancesRequest termInstanceReq = new TerminateInstancesRequest();
                     termInstanceReq.withInstanceIds(instanceInfo.getInstanceId());
                     ec2.terminateInstances(termInstanceReq);
 
-                    while (!instanceInfo.isTerminated())
+                    while (!instanceInfo.isTerminated()) {
+                        System.out.println("[AST] > Waiting for termination of instance " + instanceInfo.getInstanceId());
                         Thread.sleep(5000);
+                    }
 
-                    System.out.println("> Instance " + instanceInfo.getInstanceId() + " terminated");
+                    System.out.println("[AST] > Instance " + instanceInfo.getInstanceId() + " terminated");
 
                     instances.remove(instanceInfo.getInstanceId());
 
@@ -256,17 +270,20 @@ public class InstanceController {
                         int currHealthStatus = instance.getFailedPings();
 
                         if (pingInstance(url) != HttpURLConnection.HTTP_OK) {
+                            System.out.println("[HC] > Ping to " + instance.getInstanceId() + " Failed");
                             instance.addFailedPing();
                         } else if (currHealthStatus > 0) {
+                            System.out.println("[HC] > Ping to " + instance.getInstanceId() + " Successful");
                             instance.removeFailedPing();
                         }
 
                         if (instance.isUnhealthy()) {
                             instance.setPendingTermination();
+                            terminateInstance(instance);
                         }
 
                     } catch (MalformedURLException e) {
-                        System.out.println("> ERROR : Malformed URL!");
+                        System.out.println("[HC] > ERROR : Malformed URL!");
                     }
                 }
             }
@@ -284,7 +301,7 @@ public class InstanceController {
             return connection.getResponseCode();
 
         } catch(SocketTimeoutException ste) {
-            System.out.println("> ERROR : Connection Timed out!");
+            System.out.println("[HC] > ERROR : Connection Timed out!");
             return HttpURLConnection.HTTP_CLIENT_TIMEOUT;
         } catch (IOException ioe) {
             ioe.printStackTrace();
@@ -321,10 +338,11 @@ public class InstanceController {
             final long threadId = Thread.currentThread().getId();
             final String query = t.getRequestURI().getQuery();
             final String[] queryPairs = query.split("&");
-            int x0, x1, y0, y1;
+            int x0 = 0, x1 = 0, y0 = 0, y1 = 0;
 
-            String strategy = null;
-            Integer scanAreaSize = null;
+            String strategy = GRID_SCAN;
+
+            System.out.println(String.format("[LB-%s] > Started thread %s", threadId, threadId));
 
             for (String queryPair : queryPairs) {
                 String[] queryParameter = queryPair.split("=");
@@ -348,7 +366,7 @@ public class InstanceController {
                 }
             }
 
-            int workloadLevel = getWorkloadLevel();
+            int workloadLevel = getRequestComplexity(strategy, x1-x0, y1-y0);
             InstanceInfo instanceInfo = instanceToSendTo();
             instanceInfo.addPendingRequest(String.format("%d:%s", threadId, query), workloadLevel);
 
@@ -362,6 +380,8 @@ public class InstanceController {
                 connection.setConnectTimeout(5000);
 
                 int status = connection.getResponseCode();
+
+                System.out.println(String.format("[LB-%s] > Connection established", threadId));
 
                 if (status == HttpURLConnection.HTTP_OK) {
                     final InputStream in = connection.getInputStream();
@@ -377,6 +397,8 @@ public class InstanceController {
                     }
 
                     os.close();
+
+                    System.out.println(String.format("[LB-%s] > Response sent", threadId));
                 }
 
             } catch (MalformedURLException mue) {
@@ -389,9 +411,76 @@ public class InstanceController {
             }
         }
 
-        // TODO : Merge Ana
-        private static int getWorkloadLevel() {
+        private static int getRequestComplexity(String strategy, int width, int height) {
+            try {
+                Table table = dynamoDB.getTable(TABLE_NAME);
+                int margin = strategy.equals(GRID_SCAN) ? 20 : 40;
+
+                QuerySpec spec = new QuerySpec()
+                        .withKeyConditionExpression("Strategy = :v_strategy and Area between :v_area_min and :v_area_max")
+                        .withValueMap(new ValueMap()
+                                .withString(":v_strategy", strategy)
+                                .withNumber(":v_area_min", (width - margin) * (height - margin))
+                                .withNumber(":v_area_max", (width + margin) * (height + margin)));
+
+                ItemCollection<QueryOutcome> items = table.query(spec);
+
+                int area = width * height;
+                int diffAreas, approxArea = area;
+                Item itemToLockup = null;
+
+                for (Item item : items) {
+                    System.out.println(item.toJSONPretty());
+                    diffAreas = Math.abs(area - item.getInt("Area"));
+
+                    if (diffAreas < approxArea) {
+                        approxArea = diffAreas;
+                        itemToLockup = item;
+                    }
+                }
+
+                return itemToLockup != null ? itemToLockup.getInt("Complexity") : getDefaultRequestComplexity(strategy, area);
+
+            } catch (AmazonServiceException ase) {
+                System.out.println("Caught an AmazonServiceException, which means your request made it "
+                        + "to AWS, but was rejected with an error response for some reason.");
+                System.out.println("Error Message:    " + ase.getMessage());
+                System.out.println("HTTP Status Code: " + ase.getStatusCode());
+                System.out.println("AWS Error Code:   " + ase.getErrorCode());
+                System.out.println("Error Type:       " + ase.getErrorType());
+                System.out.println("Request ID:       " + ase.getRequestId());
+            } catch (AmazonClientException ace) {
+                System.out.println("Caught an AmazonClientException, which means the client encountered "
+                        + "a serious internal problem while trying to communicate with AWS, "
+                        + "such as not being able to access the network.");
+                System.out.println("Error Message: " + ace.getMessage());
+            }
+
             return 0;
+        }
+
+        private static int getDefaultRequestComplexity(String strategy, int area) {
+            if (area <= 10000) {
+                return 1;
+            } else if (area <= 100000) {
+                return 2;
+            } else if (area <= 150000) {
+                return 3;
+            } else if (area <= 250000) {
+                return 4;
+            } else if (area <= 350000) {
+                return 5;
+            } else if (area <= 500000) {
+                return 6;
+            } else if (area <= 800000) {
+                return 7;
+            } else if (area <= 1000000) {
+                return 8;
+            } else if (area <= 2000000) {
+                return 9;
+            } else {
+                return 10;
+            }
         }
 
         private static InstanceInfo instanceToSendTo() {
